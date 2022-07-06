cc9a923aa79116443f1416c52f17cf65cde3ff89
Elochukwu Aroh <elochukwuaroh@gmail.com>
2022-07-06 11:25:29 -0500

    Add project files.

---
+++ RDBMSACarShow/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RDBMSACarShow.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
